bd93dbf660712f0b5d9f0f41c23627586c335090
IT <89004035+BluedaleIT@users.noreply.github.com>
2023-07-28 17:36:28 +0800

    Add files via upload

---
+++ medical_services.php
@@ -0,0 +1,460 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3696733888071014"
+     crossorigin="anonymous"></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E9E0MJM664"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-E9E0MJM664');
+</script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157594434-12"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-157594434-12');
+</script>
+
+	<!--Ad Sense-->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Seremban the Guide - Medical Services</title>
+	<meta name="keywords" content="">
+	<meta name="description" content="This page consists of Perak guide book from Bluedale Group of companies">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+	
+	<link href="css/styleB.css" rel="stylesheet">
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<!-- CSS for the page -->
+	<link rel="stylesheet" href="css/subsection.css">
+	<link rel="stylesheet" href="css/entrypage6(new).css">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css">
+	<!-- Responsive CSS -->
+	<link rel="stylesheet" href="css/responsive.css">
+	<!-- Ads CSS -->
+	<link rel="stylesheet" href="css/ads.css">
+	<!-- Header and Footer CSS-->
+	<link rel="stylesheet" href="css/header_footer.css">
+	<!--Owl bootstrap -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Site Icons -->
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    
+    <!-- importing Google fonts-->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster" type="text/css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Potta+One" type="text/css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Courgette" type="text/css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik" type="text/css">
+
+<!--Tab bootstrap-->
+
+
+ <!-- Latest compiled and minified CSS -->
+
+ <!-- jQuery library -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ 
+ <!-- Popper JS -->
+ <!-- Ad-Sense -->
+ <script data-ad-client="ca-pub-3696733888071014" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+<!-- CSS for Ad-Sense -->
+<link rel="stylesheet" href="css/ads.css">
+
+ 
+ <!-- Latest compiled JavaScript -->
+
+	<style>
+		.owl-prev {
+			left: 5px;
+		}
+
+		.owl-next {
+			right: 5px;
+		}
+
+		.owl-prev, .owl-next {
+			position: absolute;
+			top: 15%;
+		}
+
+		.owl-prev span, .owl-next span {
+			font-size: 60px;
+			color: #787878;
+		}
+
+		.owl-theme .owl-nav [class*="owl-"]:hover {
+			background-color: transparent;
+		}
+
+		.blue
+      {
+        color: white;
+        background-color: blue;
+      }
+
+      #main_video::before{
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        content: '';
+        background-color: rgba(0,0,0,0.25);
+      }
+
+	  
+	</style>
+	<!-- Facebook Pixel Code -->
+
+	<!-- End Facebook Pixel Code -->
+	
+</head>
+<body style="background-color:#efefef; background-image: url(imagesv2/medical/background.jpg);
+background-repeat: no-repeat; background-size: 100% 100%; background-position: center; background-attachment: fixed;">
+
+<!--Start Header-->
+<?php include_once('includes/header.php');?>
+<!--End Header-->
+
+<!--Subsection Tab-->
+<div class="subsection">
+	<a href="places_to_stay.php" class="subsection_tab_active">
+		<img src="imagesv2/places_to_stay/royale_chulan.jpg">
+		<p>Places to Stay</p>
+	</a>
+	
+	<a href="medical_services.php" class="subsection_tab">
+		<img src="imagesv2/medical/columbia.jpg">
+		<p>Medical Services</p>
+	</a>
+	
+	<a href="places_of_worship.php" class="subsection_tab">
+		<img src="imagesv2/places_of_worship/masjid_sendayan.jpg">
+		<p>Places of Worship</p>
+	</a>
+</div>
+
+<h1>Medical Services</h1>
+
+<div class="rounded_top"></div>
+
+<div class="container2">
+
+	<div style="padding-top:0px; width:100%;" >
+
+	
+		<div class="tab-content">
+	
+			<!--Mosque Section-->
+			<div class="tab-pane fade in active show bg-white text-dark active" id="section-one" style="background-color: inherit !important;">
+
+				<div class="container_for_listing">
+
+					  <div class="listing_entry">
+						<img src="imagesv2/medical/kpj.png" class="listing_entryimage" alt="Jamek Mosque, Kamunting">
+						<div class="listing_entrycontent">
+            <h2>KPJ Seremban Specialist Hospital</h2>
+              <p>
+							<b>Location: </b><a href="https://g.page/kpj_seremban_specialist?share" target="blank"><br>
+							Lot 6219 & 6220, Jalan Toman 1, Kemayan Square, 70200 Seremban, Negeri Sembilan</a><br>
+						  	<b>Contact Number:</b><br>
+							  +606-768 6000<br>
+							  <b>Website:</b><br>
+							  <a href="   https://kpjhealth.com.my/seremban/" target="blank">
+							   https://kpjhealth.com.my/seremban/</a><br>
+              <b>Operating Hours:</b><br>24 hours (Monday-Sunday)<br>
+						  </p>
+						</div>
+					  </div>
+				  
+
+            <div class="listing_entry">
+						<img src="imagesv2/medical/sehat.jpg" class="listing_entryimage" alt="Tapah District Mosque">
+						<div class="listing_entrycontent">
+						  <h2 style="font-size:24px">Sehat Wellness & Medical Centre</h2>
+              <p>
+							<b>Location: </b><a href="https://g.page/SEHATLaserproEyeCentre?share" target="blank"><br>
+							Uptown Avenue, 352, Jalan S2 B9, Seremban 2, 70300 Seremban, Negeri Sembilan</a><br>
+						  	<b>Contact Number:</b><br>
+							  +606-601 6663 <br>
+							  <b>Website:</b><br>
+							  <a href=" https://www.sehathealthcare.com.my/" target="blank">
+							 https://www.sehathealthcare.com.my/</a><br>
+              <b>Operating Hours:</b><br>8.30 am - 6.00 pm (Monday-Saturday) <br> Closed on Sunday<br>
+						  </p>
+						</div>
+					  </div>
+            <div class="listing_entry">
+						<img src="imagesv2/medical/columbia.jpg" class="listing_entryimage" alt="Jamek Mosque, Kamunting">
+						<div class="listing_entrycontent">
+            <h2 style="font-size:24px">Columbia Asia Medical Centre</h2>
+              <p>
+							<b>Location: </b><a href="https://goo.gl/maps/nzi4ckf1cazL6S9QA" target="blank"><br>
+							292 & Lot PT1904, Jalan Haruan 2, Oakland Commercial Center, 70300 Seremban, Negeri Sembilan</a><br>
+						  	<b>Contact Number:</b><br>
+							  +606 603 3988 <br>
+							  <b>Website:</b><br>
+							  <a href="  https://www.columbiaasia.com/malaysia" target="blank">
+							  https://www.columbiaasia.com/malaysia</a><br>
+              <b>Operating Hours:</b><br>24 hours (Monday-Sunday)<br>
+						  </p>
+						</div>
+					  </div>
+				  
+					<div class="listing_entry">
+						<img src="imagesv2/medical/senawang.jpg" class="listing_entryimage" alt="Tapah District Mosque">
+						<div class="listing_entrycontent">
+						  <h2>SALAM Senawang Specialist Hospital</h2>
+              <p>
+							<b>Location: </b><a href="https://goo.gl/maps/RczU2vvAzAJ66YPw7" target="blank"><br>
+							234-243, Jalan Lavender Heights 2, Taman Lavender Heights, 70450 Senawang, Negeri Sembilan</a><br>
+						  	<b>Contact Number:</b><br>
+							  +606-675 1188<br>
+							  <b>Website:</b><br>
+							  <a href="   https://www.salamsenawang.com/" target="blank">
+							   https://www.salamsenawang.com/</a><br>
+              <b>Operating Hours:</b><br>24 hours (Monday-Sunday)<br>
+						  </p>
+						</div>
+					  </div>
+
+            <div class="listing_entry">
+						<img src="imagesv2/medical/mawar.png" class="listing_entryimage" alt="Tapah District Mosque">
+						<div class="listing_entrycontent">
+						  <h2>Mawar Specialist Medical Centre</h2>
+						  <p>
+							<b>Location: </b><a href="https://goo.gl/maps/i2LGve6y82kLpkSaA" target="blank"><br>
+							71, Jalan Rasah, 70300 Seremban, Negeri Sembilan</a><br>
+						  	<b>Contact Number:</b><br>
+							  +606-764 7048<br>
+							  <b>Website:</b><br>
+							  <a href="   https://mawarmedical.com/" target="blank">
+							   https://mawarmedical.com/</a><br>
+              <b>Operating Hours:</b><br>24 hours (Monday-Sunday)<br>
+						  </p>
+						</div>
+					  </div>
+
+					  <div class="listing_entry">
+						<img src="imagesv2/medical/NMC.jpg" class="listing_entryimage" alt="Tapah District Mosque">
+						<div class="listing_entrycontent">
+						  <h2>Aurelius Hospital Nilai (formerly known as Nilai Medical Centre)</h2>
+						  <p>
+							<b>Location: </b><a href="https://g.page/nilaimedcentre?share" target="blank"><br>
+							PT 13717 Jalan BBN 2/1, Bandar Baru Nilai, 71800 Nilai, Negeri Sembilan</a><br>
+						  	<b>Contact Number:</b>
+							   +606 850 5000<br>
+							  <b>Website:</b>
+							  <a href="http://www.nilaimc.com/" target="blank">
+							   http://www.nilaimc.com/</a><br>
+              <b>Operating Hours:</b> 24 hours (Monday-Sunday)<br>
+						  </p>
+						</div>
+					  </div>
+
+					  <div class="listing_entry">
+						<img src="imagesv2/medical/UCSI.jpg" class="listing_entryimage" alt="Tapah District Mosque">
+						<div class="listing_entrycontent">
+						  <h2>UCSI Hospital</h2>
+						  <p>
+							<b>Location: </b><a href="https://goo.gl/maps/m8R2MeAnj5wbwzgt6" target="blank"><br>
+							2, Avenue, 3, Persiaran Springhill, 71010 Port Dickson, Negeri Sembilan</a><br>
+						  	<b>Contact Number:</b><br>
+							  +606 648 8888<br>
+							  <b>Website:</b><br>
+							  <a href="https://www.ucsihospital.com/" target="blank">
+							  https://www.ucsihospital.com/</a><br>
+              <b>Operating Hours:</b><br> 24 hours (Monday-Sunday)<br>
+						  </p>
+						</div>
+					  </div>
+
+					  <div class="listing_entry">
+						<img src="imagesv2/medical/CMH.jpg" class="listing_entryimage" alt="Tapah District Mosque">
+						<div class="listing_entrycontent">
+						  <h2>CMH Specialist Hospital (Hospital Pakar CMH)</h2>
+						  <p>
+							<b>Location: </b><a href="https://g.page/CMHSpecialist?share" target="blank"><br>
+							Jln Tun Dr. Ismail, Seremban, 70200 Seremban, Negeri Sembilan</a><br>
+						  	<b>Contact Number:</b><br>
+							  +606 763 1688<br>
+							  <b>Website:</b><br>
+							  <a href="https://www.cmhspecialist.my/" target="blank">
+							  https://www.cmhspecialist.my/</a><br>
+              <b>Operating Hours:</b><br> 24 hours (Monday-Sunday)<br>
+						  </p>
+						</div>
+					  </div>
+
+					  <div class="listing_entry">
+						<img src="imagesv2/medical/klinik_ko.jpg" class="listing_entryimage" alt="Jamek Mosque, Kamunting">
+						<div class="listing_entrycontent">
+              <h2>Klinik Dr Ko</h2>
+              <p>
+							<b>Location: </b><a href="https://goo.gl/maps/Bvkyk3MZ95sFYsVQA" target="blank"><br>
+							345, Jalan SS 2B8, Uptown Avenue Seremban 2, 70300 Seremban, Negeri Sembilan</a><br>
+						  	<b>Contact Number:</b><br>
+							  +606-601 5982<br>
+							<b>Website:</b><br>
+							<a href="https://www.koskinspecialist.com/" target="blank">
+							https://www.koskinspecialist.com/</a><br>
+              <b>Operating Hours:</b><br>10.00 am - 7.00 pm (Monday-Friday) <br> 10.00 am - 6.00 pm (Saturday) <br> Closed on Sunday<br>
+						  </p>
+						</div>
+					  </div>
+
+					  <div class="listing_entry">
+					  <img src="imagesv2/medical/alpro.jpg" class="listing_entryimage" alt="Ubudiah Mosque Kuala Kangsar">
+					  <div class="listing_entrycontent">
+						<h2>Alpro Pharmacy</h2>
+						<p>
+							<b>Location: </b><a href="https://g.page/Alpro-Bandartunggal?share" target="blank"><br>
+							35 & 36, Jalan Dato Bandar Tunggal, Bandar Seremban, 70000 Seremban, Negeri Sembilan</a><br>
+						  	<b>Contact Number:</b><br>
+							  +6019-448 1923 <br>
+							<b>Website:</b><br>
+							<a href="https://www.alpropharmacy.com/oneclick/" target="blank">
+							https://www.alpropharmacy.com/oneclick/</a><br>
+              <b>Operating Hours:</b><br>8.30 am - 8.30 pm (Monday-Sunday)<br>
+						  </p>
+					  </div>
+					  </div>
+		
+					  <div class="listing_entry">
+						<img src="imagesv2/medical/multicare.jpg" class="listing_entryimage" alt="Panglima Kinta Mosque">
+						<div class="listing_entrycontent">
+						  <h2>Multicare Pharmacy</h2>
+              <p>
+							<b>Location: </b><a href="https://goo.gl/maps/aDVphEN87Atffz4R9" target="blank"><br>
+							21, Jalan S2 B18, Biz Avenue, 70300 Seremban, Negeri Sembilan</a><br>
+						  	<b>Contact Number:</b><br>
+							  +6012-630 1963<br>
+							  <b>Website:</b><br>
+							<a href="https://multicare2u.com.my/" target="blank">
+							https://multicare2u.com.my/</a><br>
+              <b>Operating Hours:</b><br>8.00 am - 8.00 pm (Monday-Sunday)<br>
+						  </p>
+						</div>
+					  </div>
+		
+					<div class="listing_entry">
+						<img src="imagesv2/medical/seremban_premier.jpg" class="listing_entryimage" alt="Muhammadiah Mosque (Masjid Cina),Ipoh">
+						<div class="listing_entrycontent">
+						  <h2 style="font-size:24px">Seremban Premier Pharmacy</h2>
+              <p>
+							<b>Location: </b><a href="https://goo.gl/maps/CTAPxDvLw9RKwicS7" target="blank"><br>
+							106, 107, Jalan Dato Bandar Tunggal, Bandar Seremban, 70000 Seremban, Negeri Sembilan</a><br>
+						  	<b>Contact Number:</b><br>
+							  +606-767 1070 <br>
+							<b>Website:</b><br>
+							<a href=" http://www.spremierpharm.com/" target="blank">
+							 http://www.spremierpharm.com/</a><br>
+              <b>Operating Hours:</b><br>8.00 am - 9.00 pm (Monday-Friday) <br> 9.00 am - 1.00 pm (Saturday-Sunday)<br>
+						  </p>
+						</div>
+					  </div>
+				  
+	
+	</div>
+
+
+</div>
+
+		
+<!--Start Footer-->	
+<?php include_once('includes/footer.php');?>
+<!--End Footer-->
+
+<script src="javascript/video.js"></script>
+
+
+<!--owl boostrap-->
+<script src="jquery.js"></script>
+	<script src="owl.carousel.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+
+<script>
+	 $(document).ready(function(){
+		   $(window).on('load', function(){
+   $('.owl-carousel2').owlCarousel({
+	   
+	   loop: true,
+	   margin: 20,
+	   nav: true,
+	   dots: false,
+	   items: 3,
+	   autoplay: true,
+	   autoplayTimeout: 9000,
+	   stagePadding: 5,
+	   responsive: {
+			   0: { items: 1
+		   },
+		   576: { items: 2
+		   },
+
+			   768: {items: 3
+		   }
+	   }
+   })
+	  
+   });
+   });
+</script>
+
+<script>
+   $(document).ready(function(){
+		   $(document).ready(function(){
+   $('.owl-carousel1').owlCarousel({
+	   
+	   loop: true,
+	   margin: 5,
+	   nav: true,
+	   dots: false,
+	   items: 5,
+	   autoplay: true,
+	   autoplayTimeout: 9000,
+	   stagePadding: 5,
+	   lazyLoad: true,
+	   responsive: {
+			   0: { items: 3
+		   },
+			   576: { items: 5
+		   },
+
+			   768: {items: 5
+		   },
+			   992: {items: 5
+		   }
+	   }
+   })
+	
+   });
+   });
+</script>
+</body>
+</html>
